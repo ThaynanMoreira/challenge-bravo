79d63149a6c68a3af62c828f52e34fc5f972c16c
Thaynã Moreira <thayna.cruz@tvglobo.com.br>
2019-09-15 16:10:37 -0300

    Initiate express

---
+++ app/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/health', (req, res) => {
+    res.send('Server running');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}!`);
+});
