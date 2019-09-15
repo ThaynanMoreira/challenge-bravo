3a4b71bd3df1fff373000cccd6a0f98c6328ab12
Thaynã Moreira <thayna.cruz@tvglobo.com.br>
2019-09-15 19:21:47 -0300

    rename rates controller test to exchange

--- test/integration/integrations/routes/exchange.js
+++ test/integration/integrations/routes/exchange.js
@@ -1,12 +1,12 @@
 const supertest = require('supertest');
-const chai = require('chai');
+// const chai = require('chai');
 const mocha = require('mocha');
 const rewire = require('rewire');
 const app = require('../../../../app');
 
 const ratesModule = rewire('../../../../app/models/rates.js');
 
-const ratesMock = rewire('../../mocks/rates.json');
+const ratesMock = require('../../../mocks/rates.json');
 
 ratesModule.__set__('getRates', () => {
     return new Promise((res) => {
@@ -24,7 +24,7 @@ module.exports.test = () => {
         describe('Route GET /exchange', () => {
             it('Should return result 0 of an exchange', (done) => {
                 request
-                    .get('/v1/exchange?from=BRL&to=USD&amount=0')
+                    .get('/v1/exchange?from=BRL&to=JPY&amount=0')
                     .expect(200)
                     .expect(0)
                     .end(done);
